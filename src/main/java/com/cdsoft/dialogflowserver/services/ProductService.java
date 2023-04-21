b4ebabe323a05943c4160bbf11aca1aa3e264f24
BarakYedaya <barak.yedaya@shieldfc.com>
2023-04-21 05:26:40 +0300

    adding similar products feature

--- src/main/java/com/cdsoft/dialogflowserver/services/ProductService.java
+++ src/main/java/com/cdsoft/dialogflowserver/services/ProductService.java
@@ -80,7 +80,7 @@ public class ProductService {
         Map<String, String> params = new HashMap<>();
         params.put(PRICE_ENTITY, Double.toString(productDetailsDto.getPrice()));
         params.put(SUPPLY_TIME_ENTITY, productDetailsDto.getDeliveryDetails());
-        params.put(PRODUCT_DETAILS_ENTITY, productDetailsDto.getProductName());
+        params.put(PRODUCT_DETAILS_ENTITY, productDetailsDto.toString());
         if(IS_IN_STOCK == productDetailsDto.getIsInStock()){
             params.put(STOCK_ENTITY, "true");
         }
